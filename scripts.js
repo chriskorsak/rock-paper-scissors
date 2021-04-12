d4af5946ab15a0a24ec59e9a6955795ce82aece2
chriskorsak <chris@chriskorsak.com>
2021-04-12 15:04:10 -0700

    refactor playround function to be less redundant. provide error if player inputs incorrect selection. add logic for player input to work no matter the capitalization.

--- scripts.js
+++ scripts.js
@@ -10,43 +10,51 @@ function computerPlay() {
 
 // function that compares computer selection with player selection
 function playRound(playerSelection, computerSelection) {
+  const computerWins = `You lose! ${computerSelection} beats ${playerSelection}`;
+  const playerWins = `You win! ${playerSelection} beats ${computerSelection}`;
+
   if (playerSelection === computerSelection) {
     return `Tie! ${playerSelection} equals ${computerSelection}`
   }
   // player chose rock
   if (playerSelection === 'Rock') {
     if (computerSelection === 'Paper') {
-      return `You lose! ${computerSelection} beats ${playerSelection}`;
+      return computerWins;
     } else {
-      return `You win! ${playerSelection} beats ${computerSelection}`;
+      return playerWins;
     }
   }
   // player chose paper
   if (playerSelection === 'Paper') {
     if (computerSelection === 'Scissors') {
-      return `You lose! ${computerSelection} beats ${playerSelection}`;
+      return computerWins;
     } else {
-      return `You win! ${playerSelection} beats ${computerSelection}`;
+      return playerWins;
     }
   }
-  // play chose scissors
+  // player chose scissors
   if (playerSelection === 'Scissors') {
     if (computerSelection === 'Rock') {
-      return `You lose! ${computerSelection} beats ${playerSelection}`;
+      return computerWins;
     } else {
-      return `You win! ${playerSelection} beats ${computerSelection}`;
+      return playerWins;
     }
   }
+  return "You didn't input a correct answer. Try again."
 }
 
 // get player selection from prompt. reprompt if no input
 let playerSelection;
 while (!playerSelection) {
-  playerSelection = prompt("Type your selection");
+  let input = prompt("Type your selection");
+  //change to lower case
+  input = input.toLowerCase();
+  //capitalize first letter and concatenate with lower case
+  playerSelection = input[0].toUpperCase() + input.slice(1);
 }
 
 // run computer play function and store in variable
 const computerSelection = computerPlay();
 
-let output = playRound(playerSelection, computerSelection);
-console.log(output);
+let outcome = playRound(playerSelection, computerSelection);
+console.log(outcome);
